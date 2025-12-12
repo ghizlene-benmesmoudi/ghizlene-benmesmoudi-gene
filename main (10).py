373d39cd751e662411613ceb1f5634cd19038b64
Marwa.Ms <marwamstf543@gmail.com>
2025-12-13 00:03:15 +0100

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,8 @@
+# BENMESMOUDI Ghizlene
+# M1 Génétique....13/12/2025
+# Membres de projet : - BENMESMOUDI Ghizlene
+#                     - MOSTEFAOUI MARWA
+#                     - BADAOUI FATIMA ZOHRA
+#                     - MOUMENI ROUMAISSA AICHA
+#                     - BENSALAH YOUSRA
+ 
