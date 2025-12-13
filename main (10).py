d75bc014f26219a10da2d3f67841edc1b38c7876
ghizlene-benmesmoudi <ghizlenebenmesmoudi@gmail.com>
2025-12-13 15:14:14 +0100

    Update main (10).py

--- main (10).py
+++ main (10).py
@@ -24,5 +24,11 @@ print(df)
 df["Length"] = df["Sequence"].str.len()
 print("\nQuestion 2 :")
 print(df["Length"]) 
+# Question 7 : Calculer l’écart-type de GC% et Length
+ecart_type_gc = df["GC%"].std()
+ecart_type_length = df["Length"].std()
+print("\nQuestion 7 :")
+print("Écart-type GC% :", ecart_type_gc)
+print("Écart-type Length :", ecart_type_length)
 
  
