ab27599f17e8393e609b8424ea6a4282b1fd2db6
bensalahyousra <bensalahyousra00@gmail.com>
2025-12-13 14:32:23 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# BENMESMOUDI Ghizlene
+# M1 Génétique....13/12/2025
+# Membres de projet : - BENMESMOUDI Ghizlene
+#                     - MOSTEFAOUI MARWA
+#                     - BADAOUI FATIMA ZOHRA
+#                     - MOUMENI ROUMAISSA AICHA
+#                     - BENSALAH YOUSRA
+ import pandas as pd
+# Données de départ
+sequences =
+    ["ATGCGTACGTA",
+    "GCTAGCTAGGCC",
+    "ATGCGCGTAAGT",
+    "TACCGATCGTA",
+    "ATGAAAGGCTT",
+    "CGTACGTAGC",
+    "TTAACCGGAT"]
+# Question 8 : Sauvegarder le tableau final dans un fichier CSV
+df.to_csv("resultats_sequences.csv", index=False)
+print("\nQuestion 8 : Fichier CSV enregistré avec succès")
+
+
