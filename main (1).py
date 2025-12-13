ee39548b655d2b96710b21d320622ac9ba4312cd
ghizlene-benmesmoudi <ghizlenebenmesmoudi@gmail.com>
2025-12-13 14:14:34 +0100

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,24 @@
+# BENMESMOUDI Ghizlene
+# M1 Génétique....13/12/2025
+# Membres de projet : - BENMESMOUDI Ghizlene
+#                     - MOSTEFAOUI MARWA
+#                     - BADAOUI FATIMA ZOHRA
+#                     - MOUMENI ROUMAISSA AICHA
+#                     - BENSALAH YOUSRA
+ import pandas as pd
+# Données de départ
+sequences = [
+    "ATGCGTACGTA",
+    "GCTAGCTAGGCC",
+    "ATGCGCGTAAGT",
+    "TACCGATCGTA",
+    "ATGAAAGGCTT",
+    "CGTACGTAGC",
+    "TTAACCGGAT"]
+# Question 7 : Calculer l’écart-type de GC% et Length
+ecart_type_gc = df["GC%"].std()
+ecart_type_length = df["Length"].std()
+print("\nQuestion 7 :")
+print("Écart-type GC% :", ecart_type_gc)
+print("Écart-type Length :", ecart_type_length)
+
